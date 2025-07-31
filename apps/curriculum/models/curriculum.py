cdf7ce0dfc13cef83fd89ef2e6e621683a0703aa
Fabio Cordoba <fabiocordoba1@gmail.com>
2025-07-31 11:06:20 -0500

    feat: add BaseModel with UUID as primary key

--- apps/curriculum/models/curriculum.py
+++ apps/curriculum/models/curriculum.py
@@ -1,6 +1,7 @@
 from django.db import models
+from apps.core.models.base_model import BaseModel
 
 
-class Curriculum(models.Model):
+class Curriculum(BaseModel):
     name = models.CharField(max_length=100)
     profile = models.TextField(blank=True)
